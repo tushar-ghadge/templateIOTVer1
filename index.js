b89e35b27208022bf0751690e24315d1aebf2d5e
Tushar Ghadge <tushar-ghadge@users.noreply.github.com>
2016-09-20 21:31:08 -0700

    Finalise
    If any changes let me know

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 $('body').scrollspy({
-	target: '#navbar-collapsible',
-	offset: 52
+  target: '#navbar-collapsible',
+  offset: 52
 });
 /* smooth scrolling sections */
 $('#navbar-collapsible a[href*=\\#]:not([href=\\#])').click(function () {
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top - 50
-			}, 800);
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 50
+      }, 800);
             // activte animations in this section
             target.find('.animate').delay(1200).addClass("animated");
             setTimeout(function () {
-            	target.find('.animated').removeClass("animated");
+              target.find('.animated').removeClass("animated");
             }, 2000);
             return false;
         }
@@ -22,7 +22,7 @@ $('#navbar-collapsible a[href*=\\#]:not([href=\\#])').click(function () {
 });
 
 $('.carousel').carousel({
-	interval: 2000
+  interval: 2000
 })
 
 var words = [
@@ -37,7 +37,7 @@ var words = [
 ];
 
 $('#cloudTags').jQCloud(words,{
-	height: 200
+  height: 200
 });
 
 /*Daya's Section*/
@@ -45,12 +45,16 @@ var iconOpen = 'fa fa-minus';
 var iconClose = 'fa fa-plus';
 
 $(document).on('show.bs.collapse hide.bs.collapse', '.accordion', function (e) {
-	var $target = $(e.target)
-	$target.siblings('.accordion-heading')
-	.find('em').toggleClass(iconOpen + ' ' + iconClose);
-	if(e.type == 'show')
-		$target.prev('.accordion-heading').find('.accordion-toggle').addClass('active');
-	if(e.type == 'hide')
-		$(this).find('.accordion-toggle').not($target).removeClass('active');
+  var $target = $(e.target)
+  $target.siblings('.accordion-heading')
+  .find('em').toggleClass(iconOpen + ' ' + iconClose);
+  if(e.type == 'show')
+    $target.prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+  if(e.type == 'hide')
+    $(this).find('.accordion-toggle').not($target).removeClass('active');
 });
 /*Daya's Section ends */
+
+$(document).ready(function(){
+    $("[data-toggle=tooltip]").tooltip();
+});
